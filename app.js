320101fbfb71e9641e32ba5da28915800c18e36d
ip11 <prash@i.camp>
2023-10-26 13:48:40 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,21 @@
+import express from "express";
+const app = express();
+
+//Import DB Connection File
+import "./utils/dbConnect.js";
+
+const port = process.env.PORT || 8080;
+
+app.use(express.json()); //json bodyparser
+
+
+//It will serve views/index.html at / 
+app.use(express.static("views"));   //HomeRouter
+
+// app.use("*", (req, res) => {
+//     res.status(404).json({ error: "Route Not Found" })
+// });
+
+app.listen(port, () => {
+    console.log(`Server Started at ${port}`);
+});
